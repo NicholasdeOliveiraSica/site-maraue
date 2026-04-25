bc25f8633f8f45bd324ed5871848cf2eab6533e6
NicholasdeOliveiraSica <nick.sica16@gmail.com>
2026-04-25 13:56:22 -0300

    testing new branch

--- app/app.vue
+++ app/app.vue
@@ -58,6 +58,7 @@ useSeoMeta({
     <UFooter>
       <template #left>
         <p class="text-sm text-muted">
+          nicholas 
           Built with Nuxt UI • © {{ new Date().getFullYear() }}
         </p>
       </template>
